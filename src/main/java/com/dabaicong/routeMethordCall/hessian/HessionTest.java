4bb6ae77aa82a7454fbb24e34381eadf1e02762e
LiuChong <18612484680@163.com>
2016-04-13 17:50:52 +0800

    hessian测试

---
+++ src/main/java/com/dabaicong/routeMethordCall/hessian/HessionTest.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright(c)2016 quanmincai Co., Ltd.
+ * All right reserved.
+ * 创建日期:2016年4月8日
+ */
+
+package com.dabaicong.routeMethordCall.hessian;
+
+/**
+ * <b>创建日期</b>: 2016年4月8日<br/>
+ * <b>作用描述</b>: 对本文件的详细描述，原则上不能少于50字
+ * @author 刘冲
+ * @mender (文件的修改者，文件创建者之外的人)
+ * @version 1.0<br/>
+ * Remark: 认为有必要的其他信息
+ */
+
+public class HessionTest {
+
+}
